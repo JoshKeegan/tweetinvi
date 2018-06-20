d3d96c7ef0feb265cbf2607da1343c1d1366f81d
Josh Keegan <josh@visav.co.uk>
2018-06-20 12:30:16 +0100

    Basic perf test - **Do not merge**

---
+++ JsonEnumStringPerfTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi.Core.Helpers;
+using Tweetinvi.Logic.JsonConverters;
+using Tweetinvi.Logic.Wrapper;
+using Tweetinvi.Models;
+
+namespace JsonEnumStringPerfTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string JSON = "{Type:\"message_create\"}";
+
+            IJsonObjectConverter converter = new JsonObjectConverter(new JsonConvertWrapper());
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                SomeClass o = converter.DeserializeObject<SomeClass>(JSON);
+
+                if (o.Type != EventType.MessageCreate)
+                {
+                    Console.WriteLine("bork!");
+                }
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("{0}", sw.Elapsed);
+        }
+    }
+}
---
+++ JsonEnumStringPerfTest/SomeClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi.Models;
+
+namespace JsonEnumStringPerfTest
+{
+    public class SomeClass
+    {
+        public EventType Type { get; set; }
+    }
+}
